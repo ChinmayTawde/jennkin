732e70c5dd396e731e893806b2d6f1bbe2049369
chinmay.route <chinmay.tawde@routemobile.com>
2025-01-06 15:31:57 +0530

    comment

--- helloWorld.php
+++ helloWorld.php
@@ -1,6 +1,23 @@
 <?php
-echo 'hiii welcomee route';
+/*echo 'hiii welcomee route';
 $a = 10;
 $b = 20;
-print_r($a+$b);
+print_r($a+$b);*/
 ?>
+<html>
+<body>
+
+<h2>HTML Forms</h2>
+
+<form action="/action_page.php">
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value="John"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value="Doe"><br><br>
+  <input type="submit" value="Submit">
+</form>
+
+<p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
+
+</body>
+</html>
